27df084d4c6cad97857dbed2818773e140c169e1
olegtuzhilkin <ibx@inbox.ru>
2022-06-19 22:02:04 +0300

    Added HTTP1.1 request realiztion

--- main.c
+++ main.c
@@ -1,8 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <getopt.h>
+#include <string.h>
+#include <getopt.h>     //getopt_long
+
+#include <sys/socket.h>
+#include <arpa/inet.h>	//inet_addr
+
+#include <netdb.h>      //getaddrinfo
 
 #define CITY_LONG   256
+
+char* get_request(char *city)
+{
+    int socket_desc;
+    struct sockaddr_in server;
+    char *message, server_reply[2000];
+
+    struct hostent *hp;
+
+    //create socket
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_desc == -1){
+        printf("Could not create socket\n");
+        return NULL;
+    }
+
+    if((hp = gethostbyname("ya.ru")) == NULL){
+            herror("gethostbyname");
+            exit(1);
+        }
+
+    memcpy(&server.sin_addr, hp->h_addr, hp->h_length);
+   // server.sin_addr.s_addr = inet_addr("google.com");
+    server.sin_family = AF_INET;
+    server.sin_port = htons(80);
+
+    //Connect to remote server
+    if (connect(socket_desc, (struct sockaddr*)&server, sizeof(server)) < 0){
+        printf("Connection error\n");
+        return NULL;
+    }
+    printf("Connected\n");
+
+    //Send some data
+    message = "GET / HTTP/1.1\r\n\r\n";
+    if( send(socket_desc , message , strlen(message) , 0) < 0)
+    {
+        puts("Send failed");
+        return NULL;
+    }
+    puts("Data Send\n");
+
+    //Receive a reply from the server
+    if( recv(socket_desc, server_reply , 2000 , 0) < 0)
+    {
+        puts("recv failed");
+    }
+    puts("Reply received\n");
+    puts(server_reply);
+}
 
 int main(int argc, char *argv[])
 {
@@ -26,6 +82,7 @@ int main(int argc, char *argv[])
                     if (optarg != NULL){
                         sprintf(city, "%s", optarg);
                         printf("arg is %s\n", city);
+                        get_request(city);
                     }
                     else{
                         printf("You didn't enter the city\n");
